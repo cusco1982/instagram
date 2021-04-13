63d93aa56ae72eb54465090c6cd80488c696dfed
Julian <cusco2891@gmail.com>
2021-04-13 13:05:59 -0400

    1.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    count:0
+  }
+
+
+  add = () => {
+    console.log('adding')
+    let num = this.state.count +1
+    this.setState({count:num})
+  }
+  minus = () => {
+    console.log('minus')
+    let num = this.state.count -1
+    this.setState({count:num})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            How many apps can we build? {this.state.count}
+          <button onClick={() => this.add()}>add</button>
+            <button onClick={() => this.minus()}>minus</button>
+          </p>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
